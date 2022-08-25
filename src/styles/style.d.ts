10a7c034bd09de92e848bf439aa8d5262915627b
Volodymyr <diachenko.vladimir@gmail.com>
2022-08-25 14:21:27 -0700

    chore: adjust eslint for trailing commas.

--- src/styles/style.d.ts
+++ src/styles/style.d.ts
@@ -4,39 +4,39 @@ declare module 'styled-components' {
   export interface DefaultTheme {
     colors: {
       primary: {
-        $600: string,
-        $500: string,
-        $400: string,
-        $300: string,
-        $200: string,
-        $100: string,
-      },
+        $600: string;
+        $500: string;
+        $400: string;
+        $300: string;
+        $200: string;
+        $100: string;
+      };
       text: {
-        $600: string,
-        $500: string,
-        $400: string
-      }
+        $600: string;
+        $500: string;
+        $400: string;
+      };
       error: {
-        $600: string,
-      }
-    },
+        $600: string;
+      };
+    };
     typography: {
       h1: {
-        size: string,
-        lineHeight: string,
-      },
+        size: string;
+        lineHeight: string;
+      };
       h2: {
-        size: string,
-        lineHeight: string
-      },
+        size: string;
+        lineHeight: string;
+      };
       h3: {
-        size: string,
-        lineHeight: string,
-      },
+        size: string;
+        lineHeight: string;
+      };
       body: {
-        fontFamily: string,
-        lineHeight: string
-      }
-    }
+        fontFamily: string;
+        lineHeight: string;
+      };
+    };
   }
 }
