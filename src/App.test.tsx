fcd8aeef5a58d7952a699f7a72222b7beababfbc
Volodymyr <diachenko.vladimir@gmail.com>
2022-08-25 16:12:52 -0700

    fix: remove async from App.test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -4,7 +4,7 @@ import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 
 describe('App', () => {
-  test('Home rendering/navigating', async () => {
+  test('Home rendering/navigating', () => {
     render(<App />, { wrapper: BrowserRouter });
     expect(
       screen.getByText(/Welcome to Space X Launches/i)
